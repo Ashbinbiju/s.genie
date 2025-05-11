f08f086e3cf32b1864ce90e7f672c452dd6a7d92
Ashbinbiju <ashbinbijuphilip99@gmail.com>
2025-05-11 20:04:07 +0530

    Update stockgenie_local.py

--- stockgenie_local.py
+++ stockgenie_local.py
@@ -24,7 +24,7 @@ from diskcache import Cache  # For persistent caching
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
-UPDATED_USER_AGENTS = [
+USER_AGENTS = [
     # Chrome on Windows
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
