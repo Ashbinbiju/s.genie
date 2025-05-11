6eb573898a520a96ca201451c95fd5fb8c904af1
Ashbinbiju <ashbinbijuphilip99@gmail.com>
2025-05-11 21:19:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "Installing smartapi-python..."
+pip install smartapi-python==1.4.8
+echo "Downloading Spacy model..."
 python -m spacy download en_core_web_sm
+echo "Listing installed packages..."
+pip list
